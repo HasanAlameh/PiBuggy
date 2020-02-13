76e27a88697a58fedd6b74acadf1d1b977060ed8
Hasan Alameh <hfalameh@umich.edu>
2020-02-12 23:11:40 -0500

    Set pins as output and testing move forward commands.

--- controls.py
+++ controls.py
@@ -1,7 +1,6 @@
 #Hasan Alameh's PiCart
 
 #Import time & GPIO controller modules
-
 import time
 import RPi.GPIO as GP
 
@@ -10,3 +9,16 @@ GP.setmode(GP.BCM)
 
 #Disable any warnings script
 GP.setwarnings(False)
+
+#Set mode of used pins as output
+GP.setup(18,GP.OUT)
+GP.setup(22,GP.OUT)
+GP.setup(17,GP.OUT)
+GP.setup(23,GP.OUT)
+
+#Move forward 1.5 seconds
+GP.setup(17,True)
+GP.setup(18,False)
+GP.setup(22,True)
+GP.setup(23,False)
+time.sleep(1.5)
